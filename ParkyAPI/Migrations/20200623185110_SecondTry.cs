85ecab5210d06cadee142009683f0f6cf43179ac
Shahab Attarnejad <atarnezhad@gmail.com>
2020-06-23 23:29:17 +0430

    Creation of Model under name of "NationalPark"

---
+++ ParkyAPI/Migrations/20200623185110_SecondTry.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkyAPI.Migrations
+{
+    public partial class SecondTry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_NationalParks",
+                table: "NationalParks");
+
+            migrationBuilder.RenameTable(
+                name: "NationalParks",
+                newName: "Tbl_NationalPark");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tbl_NationalPark",
+                table: "Tbl_NationalPark",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tbl_NationalPark",
+                table: "Tbl_NationalPark");
+
+            migrationBuilder.RenameTable(
+                name: "Tbl_NationalPark",
+                newName: "NationalParks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_NationalParks",
+                table: "NationalParks",
+                column: "Id");
+        }
+    }
+}
---
+++ ParkyAPI/Models/NationalPark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    [Table("Tbl_NationalPark")]
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
