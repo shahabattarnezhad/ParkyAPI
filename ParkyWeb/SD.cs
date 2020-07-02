4459d2fecd4a70575463519f602007b38aad9707
Shahab Attarnejad <atarnezhad@gmail.com>
2020-07-02 19:18:47 +0430

    Adding newproject to consume the api

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44351/";
+        public static string NationalParkApiPath = APIBaseUrl + "api/v1/nationalParks";
+        public static string TrailApiPath = APIBaseUrl + "api/v1/trails";
+    }
+}
