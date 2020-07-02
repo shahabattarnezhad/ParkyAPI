83a24379ed8190e6c2fb834a6c8104210e5fa90e
Shahab Attarnejad <atarnezhad@gmail.com>
2020-07-02 20:08:23 +0430

    Repositories

---
+++ ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string Url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string Url);
+        Task<bool> CreateAsync(string Url, T objToCreate);
+        Task<bool> UpdateAsync(string Url, T objToUpdate);
+        Task<bool> DeleteAsync(string Url, int Id);
+    }
+}
---
+++ ParkyWeb/Repository/Repository.cs
@@ -0,0 +1,127 @@
+﻿using Newtonsoft.Json;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public Repository(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<bool> CreateAsync(string Url, T objToCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, Url);
+            if(objToCreate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToCreate),Encoding.UTF8,"application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteAsync(string Url, int Id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Delete, Url + Id);
+
+            var client = _clientFactory.CreateClient();
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(string Url)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, Url);
+
+            var client = _clientFactory.CreateClient();
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<IEnumerable<T>>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<T> GetAsync(string Url, int Id)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, Url);
+
+            var client = _clientFactory.CreateClient();
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(string Url, T objToUpdate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Patch, Url);
+            if (objToUpdate != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(objToUpdate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
